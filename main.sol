85e5c035a779a539461ed0f62a7afbbee3d72317
Mr.PAPI <thesnakewitcher@gmail.com>
2023-07-14 22:45:57 -0400

    export solidity file

---
+++ main.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18 ;
+pragma abicoder v2 ;
+
+/**
+ * @title Contract
+ */
+contract Contract {
+
+    function some() public returns (uint8) {
+        return 10
+    }
+
+}
